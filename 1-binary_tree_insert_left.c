b4c441a54b72b954eab6e9c1bb67ce90bbf2042c
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-29 15:59:06 +0000

    making the codes simple

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,22 +19,16 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	if (parent->left == NULL)
-		parent->left = new_node;
-	else
-	{
+	if (parent->left != NULL)
+    {
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
-		parent->left = new_node;
-	}
+    }
+	parent->left = new_node;
 
 	return (new_node);
 }
