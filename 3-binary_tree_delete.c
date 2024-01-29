93800667a3f18cdcf31efab83ba754194829570f
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-29 16:30:28 +0000

    betty style fix

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,16 +2,16 @@
 
 /**
  * binary_tree_delete - A function that deletes an entire binary tree
- * 
+ *
  * @tree: The pointer to the root of the tree to delete
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return;
-        
-    binary_tree_delete(tree->left);
-    binary_tree_delete(tree->right);
+	if (tree == NULL)
+		return;
 
-    free(tree);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	free(tree);
 }
