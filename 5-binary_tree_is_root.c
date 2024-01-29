efec218d6f94deef6cb19007e03acff94c593ba0
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-29 17:30:01 +0000

    betty code implementation

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -2,15 +2,15 @@
 
 /**
  * binary_tree_is_root - A function that checks if a given node is a root
- * 
+ *
  * @node: The pointer to the node to check
- * 
+ *
  * Return: 1 if node is a root, otherwise 0. If node is NULL, 0.
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (node == NULL)
+	if (node == NULL)
 		return (0);
-        
-    return (node->parent == NULL);
+
+	return (node->parent == NULL);
 }
