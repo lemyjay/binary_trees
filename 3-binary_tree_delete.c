8b9f1a2d641ac43e2c73e02bd47667c86e91016d
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-29 16:26:26 +0000

    the implementation of task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - A function that deletes an entire binary tree
+ * 
+ * @tree: The pointer to the root of the tree to delete
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return;
+        
+    binary_tree_delete(tree->left);
+    binary_tree_delete(tree->right);
+
+    free(tree);
+}
