82afd98a9f82fab5499a29b5310f939a9271f156
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-30 16:42:27 +0000

    checking some instances

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -130,5 +130,5 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
     num_nodes = (1 << (left_height + 1)) - 1;
 
     /* Check if the number of nodes meets the perfect binary tree condition */
-    return (num_nodes == (int)Bbinary_tree_nodes(tree));
+    return (num_nodes == (int)binary_tree_nodes(tree));
 }
