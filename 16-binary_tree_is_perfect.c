e3a39cd5f2daf8b58903701afcfa6a9fcb3ea924
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-30 16:41:54 +0000

    checking some instances

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -47,6 +47,28 @@ size_t BT_height(const binary_tree_t *tree)
 }
 
 /**
+ * binary_tree_nodes - A function that counts the nodes with
+ * at least 1 child in a binary tree.
+ *
+ * @tree: A pointer to the root node of the tree to count the number of nodes.
+ *
+ * Return: Number of nodes with at least 1 child or 0 if tree is NULL
+ * A NULL pointer is not a node
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL || tree->right != NULL)
+		return (
+				1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right)
+				);
+
+	return (0);
+}
+
+/**
  * BT_size - A function that measures the size of a binary tree
  *
  * @tree: A pointer to the root node of the tree to measure the size
@@ -108,5 +130,5 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
     num_nodes = (1 << (left_height + 1)) - 1;
 
     /* Check if the number of nodes meets the perfect binary tree condition */
-    return (num_nodes == (int)BT_size(tree));
+    return (num_nodes == (int)Bbinary_tree_nodes(tree));
 }
