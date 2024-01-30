cafc3ea62c5b915ccce24ed81fb527d90bdb784f
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-30 12:38:24 +0000

    betty coding style

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -16,10 +16,10 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 		return (0);
 
 	while (tree->parent != NULL)
-    {
-        tree = tree->parent;
-        depth++;
-    }
+	{
+		tree = tree->parent;
+		depth++;
+	}
 
 	return (depth);
 }
