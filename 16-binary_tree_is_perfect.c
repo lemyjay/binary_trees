af2ac13706cb62e62c12bb174005dfbca2ece727
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-30 15:58:02 +0000

    betty coding style

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -49,21 +49,21 @@ size_t BT_height(const binary_tree_t *tree)
 
 /**
  * binary_tree_is_perfect - A function that checks if a binary tree is perfect.
- * 
+ *
  * @tree: A pointer to the root node of the tree to check.
- * 
- * Return: 1 if tree is perfect, 0 otherwise. 
+ *
+ * Return: 1 if tree is perfect, 0 otherwise.
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    int left_height, right_height;
+	int left_height, right_height;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    left_height = BT_height(tree->left);
-    right_height = BT_height(tree->right);
+	left_height = BT_height(tree->left);
+	right_height = BT_height(tree->right);
 
-    /* If the tree is full and the left and right subtrees have the same height */
-    return (BT_is_full(tree) && left_height == right_height);
+	/* If the tree is full and the left and right subtrees have the same height */
+	return (BT_is_full(tree) && left_height == right_height);
 }
