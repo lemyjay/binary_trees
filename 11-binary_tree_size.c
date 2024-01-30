38c2bc8a71d0855584c795d7149017bc147cc0b4
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-30 12:53:10 +0000

    betty fix and proper implementation

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - A function that measures the size of a binary tree
+ * binary_tree_size - A function that measures the size of a binary tree
  *
  * @tree: A pointer to the root node of the tree to measure the size
  *
@@ -12,5 +12,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (return 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
