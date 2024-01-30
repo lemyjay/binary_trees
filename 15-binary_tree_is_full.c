213f9adc672f0027839d728d3cddc3748ed4a56b
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-30 14:55:08 +0000

    betty coding style

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -2,24 +2,26 @@
 
 /**
  * binary_tree_is_full - A function tha checks if a binary tree is full.
- * 
+ *
  * @tree: A pointer to the root node of the tree to check
- * 
+ *
  * Return: 1 if the tree is full, 0 otherwise
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    
-    /* If the node has no children, it's a leaf and the tree is full */
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
-    
-    /* If the node has two children, check if both subtrees are full */
-    if (tree->left && tree->right)
-        return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-    
-    /* If the node has only one child, the tree is not full */
-    return (0);
+	if (tree == NULL)
+		return (0);
+
+	/* If the node has no children, it's a leaf and the tree is full */
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	/* If the node has two children, check if both subtrees are full */
+	if (tree->left && tree->right)
+		return (
+				binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right)
+				);
+
+	/* If the node has only one child, the tree is not full */
+	return (0);
 }
