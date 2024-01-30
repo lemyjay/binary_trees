2161672e3d1cb328aa39d74cd95637c669911a03
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-30 16:36:51 +0000

    checking some instances

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -33,7 +33,7 @@ int BT_is_full(const binary_tree_t *tree)
  *
  * Return: Height of the binary tree or 0 if tree is NULL.
  */
-int BT_height(const binary_tree_t *tree)
+size_t BT_height(const binary_tree_t *tree)
 {
 	size_t left_height, right_height;
 
@@ -46,15 +46,29 @@ int BT_height(const binary_tree_t *tree)
 	return (left_height > right_height ? left_height : right_height);
 }
 
+/**
+ * BT_size - A function that measures the size of a binary tree
+ *
+ * @tree: A pointer to the root node of the tree to measure the size
+ *
+ * Return: Size of the binary tree or 0 if trees is NULL
+ */
+size_t BT_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (1 + BT_size(tree->left) + BT_size(tree->right));
+}
 
 /**
- * binary_tree_is_perfect - A function that checks if a binary tree is perfect.
+ * binary_tree_is_perfect_ - A function that checks if a binary tree is perfect.
  *
  * @tree: A pointer to the root node of the tree to check.
  *
  * Return: 1 if tree is perfect, 0 otherwise.
  */
-int binary_tree_is_perfect(const binary_tree_t *tree)
+int binary_tree_is_perfect_(const binary_tree_t *tree)
 {
 	int left_height, right_height;
 
@@ -66,4 +80,33 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	/* If the tree is full and the left and right subtrees have the same height */
 	return (BT_is_full(tree) && left_height == right_height);
+}
+
+
+/**
+ * binary_tree_is_perfect - A function that checks if a binary tree is perfect.
+ *
+ * @tree: A pointer to the root node of the tree to check.
+ *
+ * Return: 1 if tree is perfect, 0 otherwise.
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+    int left_height, right_height, num_nodes;
+
+    if (tree == NULL)
+        return (0);
+
+    left_height = BT_height(tree->left);
+    right_height = BT_height(tree->right);
+
+    /* Check if the tree is full and the left and right subtrees have the same height */
+    if (!BT_is_full(tree) || left_height != right_height)
+        return (0);
+
+    /* Calculate the number of nodes */
+    num_nodes = (1 << (left_height + 1)) - 1;
+
+    /* Check if the number of nodes meets the perfect binary tree condition */
+    return (num_nodes == BT_size(tree));
 }
