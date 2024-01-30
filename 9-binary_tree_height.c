aebe17a50cbab279e10b74194dbe630298fe6a24
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-30 12:27:47 +0000

    corrections

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,13 +9,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t depth = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	while (tree->parent != NULL)
+    {
+        tree = tree->parent;
+        depth++;
+    }
 
-	return (left_height > right_height ? left_height : right_height);
+	return (depth);
 }
