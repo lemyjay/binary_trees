fa5a0f03caef35852e6b93943ff91aa805dee72b
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-30 16:48:18 +0000

    checking some instances

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -47,7 +47,7 @@ size_t BT_height(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_nodes - A function that counts the nodes with
+ * BT_nodes - A function that counts the nodes with
  * at least 1 child in a binary tree.
  *
  * @tree: A pointer to the root node of the tree to count the number of nodes.
@@ -55,55 +55,18 @@ size_t BT_height(const binary_tree_t *tree)
  * Return: Number of nodes with at least 1 child or 0 if tree is NULL
  * A NULL pointer is not a node
  */
-size_t binary_tree_nodes(const binary_tree_t *tree)
+size_t BT_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left != NULL || tree->right != NULL)
 		return (
-				1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right)
+				1 + BT_nodes(tree->left) + BT_nodes(tree->right)
 				);
 
 	return (0);
 }
-
-/**
- * BT_size - A function that measures the size of a binary tree
- *
- * @tree: A pointer to the root node of the tree to measure the size
- *
- * Return: Size of the binary tree or 0 if trees is NULL
- */
-size_t BT_size(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-
-	return (1 + BT_size(tree->left) + BT_size(tree->right));
-}
-
-/**
- * binary_tree_is_perfect_ - A function that checks if a binary tree is perfect.
- *
- * @tree: A pointer to the root node of the tree to check.
- *
- * Return: 1 if tree is perfect, 0 otherwise.
- */
-int binary_tree_is_perfect_(const binary_tree_t *tree)
-{
-	int left_height, right_height;
-
-	if (tree == NULL)
-		return (0);
-
-	left_height = BT_height(tree->left);
-	right_height = BT_height(tree->right);
-
-	/* If the tree is full and the left and right subtrees have the same height */
-	return (BT_is_full(tree) && left_height == right_height);
-}
-
 
 /**
  * binary_tree_is_perfect - A function that checks if a binary tree is perfect.
@@ -130,5 +93,5 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
     num_nodes = (1 << (left_height + 1)) - 1;
 
     /* Check if the number of nodes meets the perfect binary tree condition */
-    return (num_nodes == (int)binary_tree_nodes(tree));
+    return (num_nodes == BT_nodes(tree));
 }
