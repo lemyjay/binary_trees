4c2494e9eb91e1df150df6b7fe9bed02d53605f1
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2024-01-31 12:58:19 +0000

    betty compliant

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,32 +1,35 @@
 #include "binary_trees.h"
 
 /**
- * binary_trees_ancestor - A function that finds the lowest common ancestor of two nodes
- * 
+ * binary_trees_ancestor - A function that finds the lowest common
+ * ancestor of two nodes.
+ *
  * @first: A pointer to the first node
  * @second: A pointer to the second node
- * 
- * Return: A pointer to the lowest common ancestor node, or NULL if no common ancestor was found 
+ *
+ * Return: A pointer to the lowest common ancestor node,
+ * or NULL if no common ancestor was found
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(
+		const binary_tree_t *first, const binary_tree_t *second)
 {
-     const binary_tree_t *temp1, *temp2;
+	const binary_tree_t *temp1, *temp2;
 
-    if (first == NULL || second == NULL)
-        return (NULL);
+	if (first == NULL || second == NULL)
+		return (NULL);
 
-    temp1 = first;
-    while (temp1 != NULL)
-    {
-        temp2 = second;
-        while (temp2 != NULL)
-        {
-            if (temp1 == temp2)
-                return ((binary_tree_t *)temp1);
-            temp2 = temp2->parent;
-        }
-        temp1 = temp1->parent;
-    }
+	temp1 = first;
+	while (temp1 != NULL)
+	{
+		temp2 = second;
+		while (temp2 != NULL)
+		{
+			if (temp1 == temp2)
+				return ((binary_tree_t *)temp1);
+			temp2 = temp2->parent;
+		}
+		temp1 = temp1->parent;
+	}
 
-    return (NULL);
+	return (NULL);
 }
